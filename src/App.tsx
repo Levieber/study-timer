def3c2bbeccbcf79e038a1cbd5e2e7d0b83e095f
Levi Eber <gblevieber@gmail.com>
2022-11-19 17:47:16 -0300

    feat: List of Tasks Component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import Button from "./components/Button";
 import Form from "./components/Form";
+import List from "./components/List";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <h1>Study Timer</h1>
       <Form />
+      <List />
     </div>
   );
 }
---
+++ src/components/List/index.tsx
@@ -0,0 +1,17 @@
+export default function List() {
+  return (
+    <aside>
+      <h2>Seus estudos do dia:</h2>
+      <ul>
+        <li>
+          <h3>React</h3>
+          <span>02:00:00</span>
+        </li>
+        <li>
+          <h3>Typescript</h3>
+          <span>01:00:00</span>
+        </li>
+      </ul>
+    </aside>
+  );
+}
