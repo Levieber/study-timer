b29c2cde4da74a3deb65a8faaab6ee9d916b0fd4
Levi Eber <gblevieber@gmail.com>
2022-11-19 17:30:27 -0300

    feat: Button Component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Button from "./components/Button";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
+      <Button />
     </div>
   );
 }
