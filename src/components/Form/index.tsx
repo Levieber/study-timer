d0dcce9f847c3b41c87c0ef4a51622795ce03eb7
Levi Eber <gblevieber@gmail.com>
2022-11-22 13:00:38 -0300

    refactor: controlled inputs and submit handler

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,16 +3,44 @@ import Button from "../Button";
 import styles from "./Form.module.scss";
 
 export default class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00:00",
+  };
+
+  addTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
-      <form className={styles.newTask}>
+      <form className={styles.newTask} onSubmit={this.addTask}>
         <div className={styles.inputContainer}>
           <label htmlFor="task">Adicione um novo estudo:</label>
-          <input type="text" name="task" id="task" placeholder="O que você quer estudar?" required />
+          <input
+            value={this.state.task}
+            onChange={(ev) => this.setState({ task: ev.target.value })}
+            type="text"
+            name="task"
+            id="task"
+            placeholder="O que você quer estudar?"
+            required
+          />
         </div>
         <div className={styles.inputContainer}>
           <label htmlFor="time">Tempo de estudo:</label>
-          <input type="time" step="1" name="time" id="name" min="00:00:00" max="01:30:00" required />
+          <input
+            value={this.state.time}
+            onChange={(ev) => this.setState({ time: ev.target.value })}
+            type="time"
+            step="1"
+            name="time"
+            id="name"
+            min="00:00:00"
+            max="01:30:00"
+            required
+          />
         </div>
         <Button>Adicionar</Button>
       </form>
